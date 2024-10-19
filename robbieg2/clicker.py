28a99e4e79f2d7cff89954a55bc5344878d8c0ad
Patrick Barker <patrickbarkerco@gmail.com>
2024-10-19 14:03:05 -0600

    patch error

--- robbieg2/clicker.py
+++ robbieg2/clicker.py
@@ -25,21 +25,34 @@ logger.setLevel(int(os.getenv("LOG_LEVEL", logging.DEBUG)))
 
 COLOR_NUMBER = os.getenv("COLOR_NUMBER", "yellow")
 COLOR_CIRCLE = os.getenv("COLOR_CIRCLE", "red")
-GRID_SIZE = int(os.getenv("GRID_SIZE", 8)) # Amound of grid points equals (N-1)^2
-NUM_CLUSTERS = int(os.getenv("NUM_CLUSTERS", 10)) # Number of clusters to use for the composite method
-UPSCALE_FACTOR = int(os.getenv("UPSCALE_FACTOR", 3)) # How much we upscale the image on each step of zooming in
-FIRST_OCR_THRESHOLD = float(os.getenv("FIRST_OCR_THRESHOLD", 0.9)) # Threshold for the first OCR pass
-SECOND_OCR_THRESHOLD = float(os.getenv("SECOND_OCR_THRESHOLD", 0.7)) # Threshold for the second OCR pass
+GRID_SIZE = int(os.getenv("GRID_SIZE", 8))  # Amound of grid points equals (N-1)^2
+NUM_CLUSTERS = int(
+    os.getenv("NUM_CLUSTERS", 10)
+)  # Number of clusters to use for the composite method
+UPSCALE_FACTOR = int(
+    os.getenv("UPSCALE_FACTOR", 3)
+)  # How much we upscale the image on each step of zooming in
+FIRST_OCR_THRESHOLD = float(
+    os.getenv("FIRST_OCR_THRESHOLD", 0.9)
+)  # Threshold for the first OCR pass
+SECOND_OCR_THRESHOLD = float(
+    os.getenv("SECOND_OCR_THRESHOLD", 0.7)
+)  # Threshold for the second OCR pass
 
 
 class ZoomSelection(BaseModel):
     """Zoom selection model"""
+
     number: int = Field(..., description="The number of the selected circle")
 
 
 class CompositeSelection(BaseModel):
     """Composite selection model"""
-    number: int = Field(..., description="The number of the selected section of the composite")
+
+    number: int = Field(
+        ..., description="The number of the selected section of the composite"
+    )
+
 
 def recall_best_method_on_first_iteration(description: str) -> str:
     memory = {
@@ -53,6 +66,7 @@ def recall_best_method_on_first_iteration(description: str) -> str:
             return value
     return run_composite
 
+
 def recall_best_method_on_second_iteration(description: str) -> str:
     memory = {
         "calendar": run_grid,
@@ -64,6 +78,7 @@ def recall_best_method_on_second_iteration(description: str) -> str:
         if key in description.lower():
             return value
     return run_grid
+
 
 def find_coordinates(semdesk, description: str, text: str) -> dict:
     click_hash = hashlib.md5(description.encode()).hexdigest()[:5]
@@ -80,7 +95,7 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
 
     # - Setting up the stage
 
-    starting_img= semdesk.desktop.take_screenshots()[0]
+    starting_img = semdesk.desktop.take_screenshots()[0]
     starting_img_path = os.path.join(semdesk.img_path, f"{click_hash}_starting.png")
     starting_img.save(starting_img_path)
     bounding_boxes.append(Box(0, 0, starting_img.width, starting_img.height))
@@ -97,13 +112,23 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
         )
         ocr_results = find_all_text_with_bounding_boxes(starting_img_path)
 
-        best_matches = [box for box in ocr_results if similarity_ratio(box['text'], search_text) >= FIRST_OCR_THRESHOLD]
+        best_matches = [
+            box
+            for box in ocr_results
+            if similarity_ratio(box["text"], search_text) >= FIRST_OCR_THRESHOLD
+        ]
         if len(best_matches) == 1:
             best_match = best_matches[0]
             x_mid = best_match["x"] + best_match["w"] // 2
             y_mid = best_match["y"] + best_match["h"] // 2
-            bounding_boxes.append(Box(best_match['x'], best_match['y'], best_match['x'] + best_match['w'], 
-                                      best_match['y'] + best_match['h']))
+            bounding_boxes.append(
+                Box(
+                    best_match["x"],
+                    best_match["y"],
+                    best_match["x"] + best_match["w"],
+                    best_match["y"] + best_match["h"],
+                )
+            )
             semdesk.task.post_message(
                 role="Clicker",
                 msg=f"Found best matching text: '{best_match['text']}'",
@@ -120,10 +145,7 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
                 thread="debug",
                 images=[image_to_b64(debug_img)],
             )
-            return {
-                "x": x_mid,
-                "y": y_mid
-            }
+            return {"x": x_mid, "y": y_mid}
         else:
             semdesk.task.post_message(
                 role="Clicker",
@@ -136,7 +158,7 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
             msg="No text to look for in the starting image.",
             thread="debug",
         )
-    
+
     # - Finding a region where the element of interest is located
 
     semdesk.task.post_message(
@@ -144,13 +166,15 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
         msg="Looking for a region of interest...",
         thread="debug",
     )
-    region_of_interest, bounding_box = method(semdesk, starting_img, starting_img_path, description, click_hash, "region")
+    region_of_interest, bounding_box = method(
+        semdesk, starting_img, starting_img_path, description, click_hash, "region"
+    )
 
     # Escape exit, if we didn't find the region of interest because the element is not on a screen:
     # we fall back to bruteforce method
     if region_of_interest is None:
         return backup_find_coordinates(semdesk, description)
-    
+
     region_of_interest_b64 = image_to_b64(region_of_interest)
     semdesk.task.post_message(
         role="Clicker",
@@ -159,7 +183,9 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
         images=[region_of_interest_b64],
     )
     bounding_boxes.append(bounding_box)
-    region_of_interest_path = os.path.join(semdesk.img_path, f"{click_hash}_region_of_interest.png")
+    region_of_interest_path = os.path.join(
+        semdesk.img_path, f"{click_hash}_region_of_interest.png"
+    )
     region_of_interest.save(region_of_interest_path)
 
     # - OCR on the region we found
@@ -170,11 +196,23 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
             thread="debug",
         )
         zoomed_region_of_interest = region_of_interest.copy()
-        zoomed_region_of_interest = zoomed_region_of_interest.resize((zoomed_region_of_interest.width * UPSCALE_FACTOR, zoomed_region_of_interest.height * UPSCALE_FACTOR), resample=0)
-        zoomed_region_of_interest_path = os.path.join(semdesk.img_path, f"{click_hash}_zoomed_region_of_interest.png")
+        zoomed_region_of_interest = zoomed_region_of_interest.resize(
+            (
+                zoomed_region_of_interest.width * UPSCALE_FACTOR,
+                zoomed_region_of_interest.height * UPSCALE_FACTOR,
+            ),
+            resample=0,
+        )
+        zoomed_region_of_interest_path = os.path.join(
+            semdesk.img_path, f"{click_hash}_zoomed_region_of_interest.png"
+        )
         zoomed_region_of_interest.save(zoomed_region_of_interest_path)
         ocr_results = find_all_text_with_bounding_boxes(zoomed_region_of_interest_path)
-        best_matches = [box for box in ocr_results if similarity_ratio(box['text'], search_text) >= SECOND_OCR_THRESHOLD]
+        best_matches = [
+            box
+            for box in ocr_results
+            if similarity_ratio(box["text"], search_text) >= SECOND_OCR_THRESHOLD
+        ]
 
         # We trust OCR only of exactly one match over the threshold is found. Otherwise, we fall back to Grid/Composite.
         if len(best_matches) != 1:
@@ -183,11 +221,17 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
                 msg=f"No sufficiently similar text found. Found {len(best_matches)} ({best_matches})'",
                 thread="debug",
             )
-        else:            
+        else:
             best_match = best_matches[0]
-            relative_box = Box(best_match['x'], best_match['y'], best_match['x'] + best_match['w'], 
-                                        best_match['y'] + best_match['h'])
-            absolute_box = relative_box.to_absolute_with_upscale(bounding_boxes[-1], UPSCALE_FACTOR)
+            relative_box = Box(
+                best_match["x"],
+                best_match["y"],
+                best_match["x"] + best_match["w"],
+                best_match["y"] + best_match["h"],
+            )
+            absolute_box = relative_box.to_absolute_with_upscale(
+                bounding_boxes[-1], UPSCALE_FACTOR
+            )
             x_mid, y_mid = absolute_box.center()
             bounding_boxes.append(absolute_box)
             semdesk.task.post_message(
@@ -206,10 +250,7 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
                 thread="debug",
                 images=[image_to_b64(debug_img)],
             )
-            return {
-                "x": x_mid,
-                "y": y_mid
-            }
+            return {"x": x_mid, "y": y_mid}
 
     # - Two passes of Grid/Composite + Zoom
 
@@ -217,11 +258,17 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
     method = recall_best_method_on_second_iteration(description)
 
     region = region_of_interest.copy()
-    region = region.resize((region.width * UPSCALE_FACTOR, region.height * UPSCALE_FACTOR), resample=0)
-    region_of_interest_path = os.path.join(semdesk.img_path, f"{click_hash}_region_of_interest_zoom_1.png")
+    region = region.resize(
+        (region.width * UPSCALE_FACTOR, region.height * UPSCALE_FACTOR), resample=0
+    )
+    region_of_interest_path = os.path.join(
+        semdesk.img_path, f"{click_hash}_region_of_interest_zoom_1.png"
+    )
     region.save(region_of_interest_path)
     total_upscale *= UPSCALE_FACTOR
-    new_region_of_interest, relative_bounding_box = method(semdesk, region, region_of_interest_path, description, click_hash, "zoom_1")
+    new_region_of_interest, relative_bounding_box = method(
+        semdesk, region, region_of_interest_path, description, click_hash, "zoom_1"
+    )
 
     # Escape exit, if we didn't find the region of interest because the element is not on a screen:
     # we fall back to bruteforce method
@@ -232,13 +279,19 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
         bounding_boxes[-1], total_upscale
     )
     bounding_boxes.append(absolute_box_zoomed)
-    
+
     region = new_region_of_interest.copy()
-    region = region.resize((region.width * UPSCALE_FACTOR, region.height * UPSCALE_FACTOR), resample=0)
-    region_of_interest_path = os.path.join(semdesk.img_path, f"{click_hash}_region_of_interest_zoom_2.png")
+    region = region.resize(
+        (region.width * UPSCALE_FACTOR, region.height * UPSCALE_FACTOR), resample=0
+    )
+    region_of_interest_path = os.path.join(
+        semdesk.img_path, f"{click_hash}_region_of_interest_zoom_2.png"
+    )
     region.save(region_of_interest_path)
     total_upscale *= UPSCALE_FACTOR
-    last_region_of_interest, relative_bounding_box = method(semdesk, region, region_of_interest_path, description, click_hash, "zoom_2")
+    last_region_of_interest, relative_bounding_box = method(
+        semdesk, region, region_of_interest_path, description, click_hash, "zoom_2"
+    )
 
     # Escape exit, if we didn't find the region of interest because the element is not on a screen:
     # we fall back to bruteforce method
@@ -260,23 +313,19 @@ def find_coordinates(semdesk, description: str, text: str) -> dict:
 
     # LAST POINT OF POTENTIAL RETURN - WE ALWAYS RETURN SOMETHING FROM HERE, UNLESS THERE WAS AN EXCEPTION
     semdesk.results["full_grid"] += 1
-    debug_img = _debug_image(
-        starting_img.copy(), bounding_boxes, (x_mid, y_mid)
-    )
+    debug_img = _debug_image(starting_img.copy(), bounding_boxes, (x_mid, y_mid))
     semdesk.task.post_message(
         role="Clicker",
         msg="Final debug img",
         thread="debug",
         images=[image_to_b64(debug_img)],
     )
-    return {
-        "x": x_mid,
-        "y": y_mid
-    }
+    return {"x": x_mid, "y": y_mid}
+
 
 def backup_find_coordinates(semdesk, description: str) -> dict:
-    # This is a backup method of finding coordinates to click on. If the core method above fails at some point, 
-    # with a region_of_interest being 0 (i.e. not found), we try ones again through the most bruteforce mechanics that we have: 
+    # This is a backup method of finding coordinates to click on. If the core method above fails at some point,
+    # with a region_of_interest being 0 (i.e. not found), we try ones again through the most bruteforce mechanics that we have:
     # running three level of Grid + Zoom In; if that fails too, then we surely get back to Big Brain and ask some questions.
     semdesk.task.post_message(
         role="Clicker",
@@ -304,11 +353,22 @@ def backup_find_coordinates(semdesk, description: str) -> dict:
         )
 
         region = region_of_interest.copy()
-        region = region.resize((region.width * UPSCALE_FACTOR, region.height * UPSCALE_FACTOR), resample=0)
-        region_of_interest_path = os.path.join(semdesk.img_path, f"{click_hash}_grid_region_{i}.png")
+        region = region.resize(
+            (region.width * UPSCALE_FACTOR, region.height * UPSCALE_FACTOR), resample=0
+        )
+        region_of_interest_path = os.path.join(
+            semdesk.img_path, f"{click_hash}_grid_region_{i}.png"
+        )
         region.save(region_of_interest_path)
         total_upscale *= UPSCALE_FACTOR
-        region_of_interest, relative_bounding_box = method(semdesk, region, region_of_interest_path, description, click_hash, "zoom_{i}")
+        region_of_interest, relative_bounding_box = method(
+            semdesk,
+            region,
+            region_of_interest_path,
+            description,
+            click_hash,
+            "zoom_{i}",
+        )
 
         # Escape exit, if we didn't find the region of interest because the element is not on a screen.
         if region_of_interest is None:
@@ -334,27 +394,28 @@ def backup_find_coordinates(semdesk, description: str) -> dict:
 
     # LAST POINT OF POTENTIAL RETURN - WE ALWAYS RETURN SOMETHING FROM HERE, UNLESS THERE WAS AN EXCEPTION
     semdesk.results["full_grid"] += 1
-    debug_img = _debug_image(
-        starting_img.copy(), bounding_boxes, (x_mid, y_mid)
-    )
+    debug_img = _debug_image(starting_img.copy(), bounding_boxes, (x_mid, y_mid))
     semdesk.task.post_message(
         role="Clicker",
         msg="Final debug img",
         thread="debug",
         images=[image_to_b64(debug_img)],
     )
-    return {
-        "x": x_mid,
-        "y": y_mid
-    }
+    return {"x": x_mid, "y": y_mid}
 
 
 def similarity_ratio(a, b):
     return SequenceMatcher(None, a.lower(), b.lower()).ratio()
 
 
-def run_grid(semdesk, starting_image: Image.Image, starting_path: str, 
-             description: str, click_hash: str, postfix: str) -> dict:
+def run_grid(
+    semdesk,
+    starting_image: Image.Image,
+    starting_path: str,
+    description: str,
+    click_hash: str,
+    postfix: str,
+) -> dict:
     img_width, img_height = starting_image.size
     starting_image_b64 = image_to_b64(starting_image)
 
@@ -407,11 +468,15 @@ def run_grid(semdesk, starting_image: Image.Image, starting_path: str,
 
     try:
         response = router.chat(
-            thread, namespace="grid", expect=ZoomSelection, agent_id="RobbieG2", retries=1
+            thread,
+            namespace="grid",
+            expect=ZoomSelection,
+            agent_id="RobbieG2",
+            retries=1,
         )
         if not response.parsed:
             raise SystemError("No response parsed from zoom")
-        
+
         semdesk.task.add_prompt(response.prompt)
 
         zoom_resp = response.parsed
@@ -431,15 +496,21 @@ def run_grid(semdesk, starting_image: Image.Image, starting_path: str,
     region_of_interest, top_left, bottom_right = zoom_in(
         starting_path, GRID_SIZE, chosen_number, 1
     )
-    bounding_box = Box(
-        top_left[0], top_left[1], bottom_right[0], bottom_right[1]
-    )
+    bounding_box = Box(top_left[0], top_left[1], bottom_right[0], bottom_right[1])
     return region_of_interest, bounding_box
 
 
-def run_composite(semdesk, starting_image: Image.Image, starting_path: str, 
-                  description: str, click_hash: str, postfix: str) -> dict:
-    composite_path = os.path.join(semdesk.img_path, f"{click_hash}_composite_{postfix}.png")
+def run_composite(
+    semdesk,
+    starting_image: Image.Image,
+    starting_path: str,
+    description: str,
+    click_hash: str,
+    postfix: str,
+) -> dict:
+    composite_path = os.path.join(
+        semdesk.img_path, f"{click_hash}_composite_{postfix}.png"
+    )
     composite_pil, bounding_boxes = create_composite(starting_path, NUM_CLUSTERS)
     composite_pil.save(composite_path)
     composite_b64 = image_to_b64(composite_pil)
@@ -480,11 +551,15 @@ def run_composite(semdesk, starting_image: Image.Image, starting_path: str,
 
     try:
         response = router.chat(
-            thread, namespace="composite", expect=CompositeSelection, agent_id="RobbieG2", retries=1
+            thread,
+            namespace="composite",
+            expect=CompositeSelection,
+            agent_id="RobbieG2",
+            retries=1,
         )
         if not response.parsed:
             raise SystemError("No response parsed from zoom")
-        
+
         semdesk.task.add_prompt(response.prompt)
 
         composite_resp = response.parsed
@@ -497,17 +572,24 @@ def run_composite(semdesk, starting_image: Image.Image, starting_path: str,
         chosen_number = composite_resp.number
     except Exception as e:
         logger.info(f"Error in analyzing composite: {e}.")
+        return None, None
 
     if chosen_number == 0:
         return None, None
 
     bounding_box = bounding_boxes[chosen_number - 1]
     top_left = (bounding_box[0], bounding_box[1])
-    bottom_right = (bounding_box[0] + bounding_box[2], bounding_box[1] + bounding_box[3])
-    region_of_interest = starting_image.crop((top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
+    bottom_right = (
+        bounding_box[0] + bounding_box[2],
+        bounding_box[1] + bounding_box[3],
+    )
+    region_of_interest = starting_image.crop(
+        (top_left[0], top_left[1], bottom_right[0], bottom_right[1])
+    )
     box = Box(top_left[0], top_left[1], bottom_right[0], bottom_right[1])
 
     return region_of_interest, box
+
 
 def _debug_image(
     img: Image.Image,
